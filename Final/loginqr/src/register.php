60ef71e6da743e668a96cc867806a0a58d8cb71e
kittitnun <kittitnun@gmail.com>
2024-07-28 18:28:37 +0700

    final

--- Final/loginqr/src/register.php
+++ Final/loginqr/src/register.php
@@ -34,25 +34,35 @@
 <html>
 <head>
     <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<?php
+    include("layout/navbar.php");
+?> 
     <form action="register.php" method="POST">
+    <div class="mb-3">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
-
+    </div>
+    <div class="mb-3">
         <label for="password">Password:</label>
         <input type="password" name="password" required><br>
-
+    </div>
+    <div class="mb-3">
         <label for="nameTH">Name (Thai):</label>
         <input type="text" name="nameTH" required><br>
-
+    </div>
+    <div class="mb-3">
         <label for="nameEN">Name (English):</label>
         <input type="text" name="nameEN" required><br>
-
+    </div>
+    <div class="mb-3">
         <label for="studentID">Student ID:</label>
         <input type="text" name="studentID" required><br>
-
-        <input type="submit" value="Register">
+    </div>
+        <input type="submit" class="btn btn-info" value="Register">
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
